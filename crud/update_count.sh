7217dc78a7649c0ebf030e2b34c5c091c469fa59
Jonathan-fadida~ <fadida.y7@gmail.com>
2023-08-24 18:43:52 +0300

    update name and count

--- crud/update_count.sh
+++ crud/update_count.sh
@@ -2,44 +2,49 @@
 
 source ./search_record.sh  
 
+function get_record_file()
+{
+	echo "../db/listing.csv"
+	return 0
+}
+
 
 function update_record_count()
 {
 
     local name=$1
-    local num=$2
-    local search_status=0
-    
+    local new_value=$2
+    local listing_path=$(get_record_file)
+    local ret_status=0
     local result_search_fun=0
     
 
-    search_record_get_single result_search_fun $name
+    search_record_get_single result_search_fun $name 
     
-    search_status="$?"
+    ret_status="$?"
     
-    if [[ "$search_status" -eq 0 ]]; then
-        echo $result_search_fun
-
-    fi
-
+    if [[ "$ret_status" -eq 0 ]]; then
+        if [[  "${new_value}" -lt 1 ]]; then
+            echo "Error, the count is unreal " >> /dev/stderr
+            ret_status=1
+        else    
+            sed -i "s/^${result_search_fun},[1-9][0-9]*/${result_search_fun},${new_value}/" "${listing_path}"
+            echo "amount updated"
+        fi         
     
+	fi
 
-
-
-
-
-	local result=0
-	local ret_status=0
-	
 	return $ret_status
 
 }
 
 
+
+
 function main()
 {
 	### workaround to handle the pitfall of local declaration changing the return status ($?) - 'update_count_func_result' must be globaly unique
-	update_count_func_result=$(update_record_count "$1" "$2")
+	update_record_count "$1" "$2"
 	local ret_status="$?"
 	###
 	local result="$update_count_func_result"
--- crud/update_name.sh
+++ crud/update_name.sh
@@ -1,52 +1,47 @@
 #! /bin/bash
 
+source ./search_record.sh  
 
-function update_record_name.sh()
+function get_record_file()
 {
-	local result=0
-	local ret_status=0
-
-    local old_name=$1
-    local new_name=$2
-
-    echo $old_name
-    echo $new_name
-
-    local RECORD_FILE="../db/listing.csv"
-
-	result="`grep "${old_name}" "${RECORD_FILE}" | sort `"
-	record_names="`echo "${result}" | cut -d "," -f 1 `"
-	
-	if [[ "${#result}" -eq 0 ]]; then
-		echo "Search failed - no such record" >> /dev/stderr
-		ret_status=1
-	else
-		echo "${result}"
-	fi
-
-
-
-
-
-
-
-
-	return $ret_status
+	echo "../db/listing.csv"
+	return 0
 }
 
 
+function update_record_count()
+{
 
+    local old_name=$1
+    local new_name=$2
+    local listing_path=$(get_record_file)
+    local ret_status=0
+    local result_search_fun=0
+    
+
+    search_record_get_single result_search_fun $old_name 
+    
+    ret_status="$?"
+    
+    if [[ "$ret_status" -eq 0 ]]; then
+            sed -i "s/^${result_search_fun},/${new_name},/" "${listing_path}"
+            echo "name updated"
+    fi  
+
+	return $ret_status
+
+}
 
 
 
 
 function main()
 {
-	### workaround to handle the pitfall of local declaration changing the return status ($?) - 'insert_record_func_result' must be globaly unique
-	insert_record_func_result=$(update_record_name.sh "$1" "$2")
+	### workaround to handle the pitfall of local declaration changing the return status ($?) - 'update_count_func_result' must be globaly unique
+	update_record_count "$1" "$2"
 	local ret_status="$?"
 	###
-	local result="$insert_record_func_result"
+	local result="$update_count_func_result"
 	
 	if [[ "$ret_status" -eq 0 ]]; then
 		echo "$result"
@@ -56,8 +51,17 @@ function main()
 }
 
 
- # in case of running as a script
+# in case of running as a script
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     main "${@}"
 fi
+
+
+
+
+
+
+
+
+
 
