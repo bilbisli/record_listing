a47a994d9e06668cfd65019b7243f91209b5c374
sawserlauz48 <sawserlauz48@gmail.com>
2023-08-24 16:05:27 +0300

    import surch function to insert record function

--- crud/insert_record.sh
+++ crud/insert_record.sh
@@ -1,22 +1,50 @@
 #! /bin/bash
 source ./search_record.sh
 
-
+function get_record_file()
+{
+	echo "../db/listing.csv"
+	return 0
+}
 
 
 function insert_record()
 {
 	
-	local record_name=$1
-	local record_amount_from_user=$2
-#	local record_amount_from_user=$2
-	local result=0
+	local record_amount=$1
+	shift
+	local record_name="$@"
 	local ret_status=0
-	local result_arr=$(search_record "$1")
-	echo "${result_arr[@]}" 
-#	echo "$(cat ../db/listing.csv)"
+	local RECORD_FILE=$(get_record_file)
+	local search_function_result=0
+	
+	
+	search_record_get_single "search_function_result" "--add" "$record_name"
+	search_status="$?"
+	echo "$search_status"
+	
+	
+	var1=$(echo $search_function_result)
+	echo "$var1"
+
+    	if [[ "$search_status" -eq 0 ]]; then
+        	echo $result_search_fun
+
+    	fi
+	
+	
+	
+	
 	
 	return $ret_status
+	
+	
+	
+
+	## if we want to insert new record
+#	echo "new_record,record_amount" >> $RECORD_FILE
+	## if we want to change the name record	
+#	echo $(cat "$RECORD_FILE")
 }
 
 
@@ -24,14 +52,9 @@ function main()
 {
 
 	### workaround to handle the pitfall of local declaration changing the return status ($?) - 'insert_record_func_result'    		must be globaly unique
-	insert_record_func_result=$(divide "$1" "$2")
+
+	insert_record  "${@}"
 	local ret_status="$?"
-	###
-	local result="$insert_record_func_result"
-	
-	if [[ "$ret_status" -eq 0 ]]; then
-		echo "$result"
-	fi
 	
 	exit "$ret_status"
 }
